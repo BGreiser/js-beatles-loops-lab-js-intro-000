c84eb6ad285bca084f9acb2431320b137232ae69
alluring-machine-4767 <bgreiser@gmail.com>
2020-08-22 20:15:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let x = [];
+  for (let i = 0; i <= musicians.length; i++){
+    x.push(`${musicians[i]} plays ${instuments[i]}`);
+  }
+  return x;
+}
